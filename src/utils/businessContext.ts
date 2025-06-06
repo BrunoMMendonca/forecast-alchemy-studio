39ed956e0a2577a5b9d37eb632ca59a885c1496c
Bruno Mendonça <40309779+BrunoMMendonca@users.noreply.github.com>
2025-06-06 20:48:07 +0100

    Deleted ForecastSummaryStats.tsx

--- src/utils/businessContext.ts
+++
@@ -1,8 +0,0 @@
-
-import { BusinessContext, DEFAULT_BUSINESS_CONTEXT } from '@/types/businessContext';
-
-export const getBusinessContext = (): BusinessContext => {
-  // For now, return the default business context
-  // This can be extended later to load from user preferences or settings
-  return DEFAULT_BUSINESS_CONTEXT;
-};
